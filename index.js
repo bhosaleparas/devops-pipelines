1012acdc8a7e09f8f460b25c59f938c96a7e1dcd
paras <bhosaleparas53@gmail.com>
2026-01-07 14:03:20 +0530

    feat: add CI pipeline

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("hello devops")
+
+console.log("I am learning CI/CD pipelines using git actions")
---
+++ test.js
@@ -0,0 +1,8 @@
+console.log("Starting tests");
+
+setTimeout(()=>
+    console.log('waiting for 3 seconds...')
+,3000);
+
+
+console.log("Test completed")
